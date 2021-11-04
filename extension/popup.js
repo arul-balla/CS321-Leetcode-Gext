99f3750f1bdc2f13db35e95047b1c29b9c7c558f
ngosm27 <56649705+ngosm27@users.noreply.github.com>
2021-11-04 00:20:01 -0400

    Update popup.js

--- extension/popup.js
+++ extension/popup.js
@@ -1,9 +1,3 @@
-let changeColor = document.getElementById("changeColor");
-
-chrome.storage.sync.get("color", ({ color }) => {
-  changeColor.style.backgroundColor = color;
-});
-
 let username = document.getElementById('username');
 let password = document.getElementById('password');
 
